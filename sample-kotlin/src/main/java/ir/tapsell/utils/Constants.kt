668933435a771d86b81a684ba76c0adc36e76a0b
Morteza Nedaei <nedaei1375@gmail.com>
2023-10-18 16:26:00 +0330

    chore: ad pre roll zone id

--- sample-kotlin/src/main/java/ir/tapsell/utils/Constants.kt
+++ sample-kotlin/src/main/java/ir/tapsell/utils/Constants.kt
@@ -6,5 +6,5 @@ object Constants {
     const val TAPSELL_REWARDED_VIDEO = "1d710cc7-5e96-46ac-a3e9-8463300333e6"
     const val TAPSELL_INTERSTITIAL = "b3972749-f62a-475a-9ff2-cfc9e2a40f87"
     const val TAPSELL_STANDARD_BANNER = "e3d5999c-5990-4e31-8ce9-642ce040a7f4"
-    const val TAPSELL_PRE_ROLL = "6977a96f-e265-4895-b219-33bb6ba3df85"
+    const val TAPSELL_PRE_ROLL = "628f60d7-8fd0-4aea-a840-2d2947121dd5"
 }
