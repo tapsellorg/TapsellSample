64840923a2879ada1a31a42cadd3a8bbeb38f134
MoNe <nedaei1375@gmail.com>
2023-12-11 14:12:29 +0330

    release: 1.0.1 beta01 (#14)
    chore: update SDK to 1.0.1-beta01

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,28 @@
 pluginManagement {
     repositories {
         google()
-        mavenCentral()
         mavenLocal()
+        mavenCentral()
         gradlePluginPortal()
+        maven("https://android-sdk.is.com")
+        maven("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
         maven("https://cboost.jfrog.io/artifactory/chartboost-mediation")
         maven("https://maven.wortise.com/artifactory/public")
-        maven("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
-        maven("https://android-sdk.is.com")
+
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+
         google()
-        mavenCentral()
         mavenLocal()
+        mavenCentral()
+        maven("https://android-sdk.is.com")
+        maven("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
         maven("https://cboost.jfrog.io/artifactory/chartboost-mediation")
         maven("https://maven.wortise.com/artifactory/public")
-        maven("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
-        maven("https://android-sdk.is.com")
+
     }
 }
 
