894822b0ed5a048fb9f3a163a772c30064e78bef
Morteza Nedaei <nedaei1375@gmail.com>
2024-05-05 23:21:42 +0330

    feat: add a workflow to generate release APK using input keys and zones

--- scripts/change-app.sh
+++ scripts/change-app.sh
@@ -6,13 +6,15 @@ update_app_keys() {
   CI_ADMOB_APP_ID="$2"
   CI_APPLOVIN_APP_ID="$3"
 
-  echo "Updating Tapsell App Keys: TapsellAppId: $CI_TAPSELL_APP_ID - AdmobAppId: $CI_ADMOB_APP_ID - ApplovinAppId: $CI_APPLOVIN_APP_ID"
+  FILE="$PROJECT_DIR/build.gradle.kts"
+
+  echo "Project Directory: $FILE"
 
   if [ -n "$CI_TAPSELL_APP_ID" ] && [ -n "$CI_ADMOB_APP_ID" ] && [ -n "$CI_APPLOVIN_APP_ID" ]; then
+      echo "Updating Tapsell App Keys: TapsellAppId: $CI_TAPSELL_APP_ID - AdmobAppId: $CI_ADMOB_APP_ID - ApplovinAppId: $CI_APPLOVIN_APP_ID"
       sed -i '' -e "s/const val TAPSELL_APP_ID.*/const val TAPSELL_APP_ID = \"$CI_TAPSELL_APP_ID\"/" "$FILE"
       sed -i '' -e "s/const val ADMOB_APP_ID.*/const val ADMOB_APP_ID = \"$CI_ADMOB_APP_ID\"/" "$FILE"
       sed -i '' -e "s/const val APPLOVIN_APP_ID.*/const val APPLOVIN_APP_ID = \"$CI_APPLOVIN_APP_ID\"/" "$FILE"
-      echo "App Keys updated successfully."
   else
       echo "Failed to update app keys: One or more keys are undefined."
   fi
@@ -25,7 +27,3 @@ else
   PROJECT_DIR=$(dirname "$PWD")
   update_app_keys "$1" "$2" "$3"
 fi
-
-FILE="$PROJECT_DIR/build.gradle.kts"
-
-echo "Project Directory: $PROJECT_DIR"
