158c6619a4d21d1ec32fd6903cd8ea527cb479a7
Morteza Nedaei <nedaei1375@gmail.com>
2024-05-25 11:38:16 +0330

    chore: update kotlin to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinJvm) apply false
+    alias(libs.plugins.composeCompiler) apply false
 }
 
 ext {
