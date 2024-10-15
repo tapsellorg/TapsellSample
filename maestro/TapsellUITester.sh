5ca7f325237b6f5f9921a498b8df63a148dd0a59
Morteza Nedaei <nedaei1375@gmail.com>
2024-10-15 14:08:23 +0330

    feat: implement Auto Smock UI test with Maestro to test all zone types

---
+++ maestro/TapsellUITester.sh
@@ -0,0 +1,20 @@
+start_time=$(date +%s)
+
+echo "Take a coffee until my tests are done! ☕️☕️☕️"
+echo "Starting Automated Smock UI Testing ..."
+
+rm -rf maestro-outputs
+
+maestro test rewarded.yaml
+maestro test interstitial.yaml
+maestro test native.yaml
+maestro test native-multiple.yaml
+maestro test standard.yaml
+maestro test preroll.yaml
+
+mv *.mp4 maestro-outputs
+
+end_time=$(date +%s)
+
+time_elapsed=$((end_time - start_time))
+echo "Time elapsed: $((time_elapsed / 60)) minutes and $((time_elapsed % 60)) seconds."
