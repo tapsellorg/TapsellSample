71339125634fde8036ca86bf920599bbee8205de
MoNe <nedaei1375@gmail.com>
2025-01-26 09:37:09 +0330

    chore: update plugin version to 1.0.2-beta04 (#66)

--- sample-jetpack-compose/src/main/java/ir/tapsell/sample/ui/screens/native_banner/NativeBannerScreen.kt
+++ sample-jetpack-compose/src/main/java/ir/tapsell/sample/ui/screens/native_banner/NativeBannerScreen.kt
@@ -2,6 +2,7 @@ package ir.tapsell.sample.ui.screens.native_banner
 
 import android.app.Activity
 import android.view.LayoutInflater
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,7 +19,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -100,7 +100,7 @@ private fun TapsellNativeBannerView(
     modifier: Modifier = Modifier,
     onViewCreated: (NativeAdViewContainer?) -> Unit = {},
 ) {
-    val context = LocalContext.current as Activity
+    val context = LocalActivity.current as Activity
     val container = NativeAdViewContainer(context)
 
     AndroidView(
